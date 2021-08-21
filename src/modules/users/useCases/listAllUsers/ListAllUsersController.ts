633b3577c942ea69156dd99142a059ffe3033597
tiago <tyagolp@gmail.com>
2021-08-21 14:15:16 -0300

    fix controller ListAllUsers

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,11 +6,10 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.body
+    const { user_id } = request.headers
     const users = this.listAllUsersUseCase.execute({user_id})
 
     return response.json(users)
-    // Complete aqui
   }
 }
 
